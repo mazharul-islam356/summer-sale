376d34622c294631c80ce2b99bced24c8b351124
unknown <mazharulislam3569@gmail.com>
2023-08-21 04:30:52 +0600

    disable and enable apply+make purchase button added

--- index.js
+++ index.js
@@ -3,14 +3,39 @@ let liItems = document.getElementById('liItems')
 
 function divClickButton (target){
     liItemName = target.childNodes[7].childNodes[1].innerText;
-    li = document.createElement('li');
-    li.innerText = liItemName;
-    liItems.append(li)
+    
+    const olName = liItems.childElementCount;
+
+    li = document.createElement('p');
+    li.innerHTML = `${olName + 1}. ${liItemName}`
+    liItems.appendChild(li)
     
     let totalPrice = target.childNodes[7].childNodes[3].innerText.split(" ")[0];
     total = parseInt(total) + parseInt(totalPrice)   
     document.getElementById('totalPriceResult').innerText = total;
     
+    // apply button disable-enable
+    let applyButton = document.getElementById('applyButton')
+    applyButton.disabled = true;
+    if(total >= 200){
+    applyButton.disabled = false;
+    }else{
+    applyButton.disabled = true;
+    }
+    // make purchase button disable-enable
+    let makePurchaseButton = document.getElementById('makePurchaseButton')
+    makePurchaseButton.disabled = true;
+    if(total >= 0){
+    makePurchaseButton.disabled = false;
+    }else{
+    makePurchaseButton.disabled = true;
+    }
+
     
+   
 }
+
+
+
+
 
